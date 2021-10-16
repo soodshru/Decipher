5020692ddc13f9348e3c181174f2d346f2503ff5
Siddhant Singh <singhsiddhant2013@gmail.com>
2021-10-16 19:11:07 -0400

    API url change

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
+const backendUrl = 'http://127.0.0.1:5000'
+
 export const sendAudio = async (file, setLoading) => {
     setLoading(true)
     let data = new FormData();
-    data.append('wavfile', file, file.name);
+    data.append('file', file, file.name);
 
     const config = {
         headers: { 'content-type': 'multipart/form-data' }
     }
     try{
-        const response = await axios.post('/api/recorderfiles', data, config)
+        const response = await axios.post(backendUrl + '/speechtotext', data, config)
         setLoading(false)
         console.log(response)
     } catch (err) {
