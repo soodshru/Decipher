719fccae9b3ab98948f18d766a1ef1a13ede7b9c
Shruti Sood <shruti.sood@mail.utoronto.ca>
2021-10-16 16:55:09 -0400

    react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Record } from './components/Record';
+
+function App() {
+  return (
+    <div className="App">
+      <Record/>
+    </div>
+  );
+}
+
+export default App;
