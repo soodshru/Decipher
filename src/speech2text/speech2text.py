b84085ff56f0b8113201acdb8de5ec345be7831b
Abhishek Chatterjee <abhi1506@live.com>
2021-10-16 18:25:59 -0400

    changed parameters to use with wav files

--- src/speech2text/speech2text.py
+++ src/speech2text/speech2text.py
@@ -3,29 +3,28 @@ import os
 # Imports the Google Cloud client library
 from google.cloud import speech
 
-def gen_text(mp3_path):
-    #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/abhishek/text-to-speech-329203-c2a0cf1d09e2.json"
+def gen_text(wav_path):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/path/to/text-to-speech-329203-c2a0cf1d09e2.json"
 
     # Instantiates a client
     client = speech.SpeechClient()
 
     # Reading byte stream from audio file
-    with open(mp3_path, 'rb') as open_file:
+    with open(wav_path, 'rb') as open_file:
         byte_data = open_file.read()
 
-    audio_mp3 = speech.RecognitionAudio(content=byte_data)
+    audio_wav = speech.RecognitionAudio(content=byte_data)
 
     # Configure media files output
-    config_mp3 = speech.RecognitionConfig(
-        sample_rate_hertz=48000,
+    config_wav = speech.RecognitionConfig(
         enable_automatic_punctuation=True,
         language_code='en-US'
     )
 
     # Transcribing RecognitionAudio object
     response = client.recognize(
-        config=config_mp3,
-        audio=audio_mp3
+        config=config_wav,
+        audio=audio_wav
     )
 
     print(response.results[0].alternatives[0].transcript)
